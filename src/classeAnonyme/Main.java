5c971335c674bb186e12afd092cb09ef3db668eb
geo-toma <nmenageorges02@gmail.com>
2019-02-10 13:36:20 +0100

    utilisation des lambda

--- src/classeAnonyme/Main.java
+++ src/classeAnonyme/Main.java
@@ -15,6 +15,13 @@ public class Main {
 		});
 		per.soigner();
 		
+		per.setSoin(() -> {System.out.println("methode soigner redefinit avec lambda");});
+		per.soigner();
+		
+		Soin soin = () -> {System.out.println("methode soigner redefinit avec lambda d'une autre maniere");};
+		per.setSoin(soin);
+		per.soigner();
+		
 	}
 
 }
